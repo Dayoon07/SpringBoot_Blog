06ff5ed78e640d882cbb82d2f048ec9d60cc31be
Dayoon07 <gangd0642@gmail.com>
2025-02-12 13:04:28 +0900

    이제 진짜 끝

--- SIGMA/src/main/java/com/e/d/controller/MainController.java
+++ SIGMA/src/main/java/com/e/d/controller/MainController.java
@@ -6,16 +6,12 @@ import java.net.InetAddress;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- SIGMA/src/main/java/com/e/d/model/service/LikeService.java
+++ SIGMA/src/main/java/com/e/d/model/service/LikeService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.e.d.model.entity.BoardEntity;
 import com.e.d.model.entity.LikeEntity;
